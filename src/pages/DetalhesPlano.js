cc62ce410469e6600b9945fbbcd3f6766b4533d0
Rony Moura <ronymmoura@gmail.com>
2018-08-27 10:53:03 -0300

    Removendo log

--- src/pages/DetalhesPlano.js
+++ src/pages/DetalhesPlano.js
@@ -45,7 +45,6 @@ export default class DetalhesPlano extends React.Component {
     }
 
     async componentDidMount() {
-        console.log(this.state.plano);
         try { 
             var result = await planoService.BuscarPorCodigo(this.state.cdPlano);
             await this.setState({ plano: result.data });
