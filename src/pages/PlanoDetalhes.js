1171237448835a2258c3fc3fea882a4940a3edb1
Luis Bruno <l_brunofidelis@yahoo.com.br>
2019-02-21 18:25:55 -0300

    Refatoração detalhes plano

--- src/pages/PlanoDetalhes.js
+++ src/pages/PlanoDetalhes.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { PlanoService, SalarioBaseService, FichaFechamentoPrevesService } from "@intechprev/prevsystem-service";
-
 import DataInvalida from './_shared/Data';
 import { Page } from ".";
-import { Box, FormFieldStatic, Row, Col } from "../components";
-
-var InputMask = require('react-input-mask');
+import { Box, FormFieldStatic, Row, Col, CampoTexto, Button, Alert, Form } from "../components";
 
 export default class DetalhesPlano extends React.Component {
     constructor(props) {
@@ -15,13 +12,6 @@ export default class DetalhesPlano extends React.Component {
             modalVisivel: false,
             dataInicio: "",
             dataFim: "",
-
-            // States de validação e mensagens de erro.
-            erroCampoVazio: false,
-            erroCampoInvalido: false,
-            mensagemErro: "",
-            erroDataInicialSuperior: false,
-            erroDataFinalSuperior: false,
 
             cdPlano: props.match.params.plano,
             plano: {
@@ -34,6 +24,8 @@ export default class DetalhesPlano extends React.Component {
             possuiSeguro: false
         }
         
+        this.form = React.createRef();
+        this.alert = React.createRef();
     }
 
     componentDidMount = async () => {
@@ -82,161 +74,94 @@ export default class DetalhesPlano extends React.Component {
                 <div className="modal" role="dialog">
                     <div className="modal-dialog modal-lg" role="document">
                         <div className="modal-content">
+
                             <div className="modal-header">
                                 <h5 className="modal-title">Período</h5>
                                 <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => this.toggleModal()}>
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
-                            <div className="modal-body">
-                                <div className="row">
-                                    <div className="col-lg-6">
-                                        <div className="form-group" align="center">
-                                            <label htmlFor="dataInicio"><b>Data de Início:</b></label>
-                                            <InputMask mask="99/99/9999" placeholder="Data inicial" name="dataInicio" id="dataInicio" type="text" className="form-control" 
-                                                   value={this.state.dataInicio} onChange={this.onChangeInput} />
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6">
-                                        <div className="form-group" align="center">
-                                            <label htmlFor="dataFim"><b>Data Final:</b></label>
-                                            <InputMask mask="99/99/9999" placeholder="Data final" name="dataFim" id="dataFim" type="text" className="form-control" 
-                                                   value={this.state.dataFim} onChange={this.onChangeInput} />
-                                        </div>
-                                    </div>
+                            
+                            <Form ref={this.form}>
+                                <div className="modal-body">
+                                    <Row>
+                                        <Col className={"col-lg-6"}>
+                                            <CampoTexto contexto={this} nome={"dataInicio"} mascara={"99/99/9999"} obrigatorio valor={this.state.dataInicio} 
+                                                        label={"Data de Início"} underline />
+                                        </Col>
+
+                                        <Col className={"col-lg-6"}>
+                                            <CampoTexto contexto={this} nome={"dataFim"} mascara={"99/99/9999"} obrigatorio valor={this.state.dataFim} 
+                                                        label={"Data Final"} underline />
+                                        </Col>
+                                    </Row>
                                 </div>
-                            </div>
-                            <div className="modal-footer">
-                                {this.state.mensagemErro !== "" &&
-                                    <div className="text-danger">
-                                        <i className="fas fa-exclamation-circle"></i>&nbsp;
-                                        {this.state.mensagemErro}
-                                    </div>
-                                }&nbsp;
-                                <button type="button" className="btn btn-primary" onClick={this.validarVazios}>Gerar</button>
-                            </div>
+
+                                <Alert ref={this.alert} padraoFormulario tipo={"danger"} tamanho={"6"} />
+                                <div className="modal-footer">
+                                    <Button id={"gerar"} titulo={"Gerar"} tipo="primary" submit onClick={this.gerarExtrato} />
+                                </div>
+                            </Form>
+
                         </div>
                     </div>
                 </div>
             );
         }
         else
-            return (<div></div>)
+            return <div></div>
 
     }
 
-    onChangeInput = (event) => {
-        var target = event.target;
-        var valor = target.value;
-        var campo = target.name;
-
-        this.setState({
-            [campo]: valor
-        })
-    }
-
-    /**
-     * Método que checa se os campos estão vazios e atualiza o state que contém essa informação. Após isso faz uma chamada de validarInvalidos().
-     */
-    validarVazios = () => {
-        // Variável que armazena true caso um dos campos esteja vazio.
-        var campoVazio = (this.state.dataInicio === "" || this.state.dataFim === "")
-
-        this.setState({
-            erroCampoVazio: campoVazio
-        }, () => { this.validarInvalidos() });
-
-    }
-
-    /**
-     * Método que valida os campos dataInicio e dataFim. Para validação das datas é utilizado uma função que valida a data para não aceitar datas 
-     * futuras e estar dentro dos limites de dias e meses. Os states são atualizados e faz-se uma chamada ao renderizaMensagemErro().
-     */
-    validarInvalidos = () => {
-        var dataInicioObjeto = this.converteData(this.state.dataInicio);
-        var dataInicioInvalida = DataInvalida(dataInicioObjeto, this.state.dataInicio);
-
-        var dataFimObjeto = this.converteData(this.state.dataFim);
-        var dataFimInvalida = DataInvalida(dataFimObjeto, this.state.dataFim);
-
-        // Variável que armazena true caso um dos campos esteja inválido.
-        var dataInvalida = dataInicioInvalida || dataFimInvalida;
-
-        if(dataInicioObjeto > dataFimObjeto) {
-            this.setState({ erroDataInicialSuperior: true })
-
-        } else if(dataFimObjeto > new Date()) {
-            this.setState({ 
-                erroDataFinalSuperior: true,
-                erroDataInicialSuperior: false
-            })
-
-        } else {
-            this.setState({
-                erroDataInicialSuperior: false,
-                erroDataFinalSuperior: false
-            })
-        }
-
-        this.setState({ 
-            erroCampoInvalido: dataInvalida 
-        }, () => { this.renderizaMensagemErro() });
-
-    }
+    gerarExtrato = async () => {
+        try {
+            await this.alert.current.limparErros();
+            await this.form.current.validar();
     
-    /**
-     * @param {string} dataString Data a ser convertida para Date().
-     * @description Método responsável por converter a data recebida (no formato 'dd/mm/aaaa') para date (Objeto).
-     */
-    converteData = (dataString) => {
-        var dataPartes = dataString.split("/");
-        return new Date(dataPartes[2], dataPartes[1] - 1, dataPartes[0]);
-    }
-
-    /**
-     * Método que altera o state 'mensagemErro' para o tipo de mensagem que deve ser mostrada para o usuário. Após isso faz uma chamada de gerarExtrato.
-     */
-    renderizaMensagemErro = () => {
-        // Mensagem de campo vazio é renderizada caso um dos dois campos esteja vazio.
-        if(this.state.erroCampoVazio) {
-            this.setState({ mensagemErro: "Preencha todos os campos!" })
-
-        // Mensagem de campo inválido é renderizada caso um dos dois campos esteja inválido.
-        } else if(this.state.erroCampoInvalido) {
-            this.setState({ mensagemErro: "Preencha todos os campos corretamente!" })
-
-        // Mensagem de data inicial superior é renderizada caso a data inicial seja superior que a data final.
-        } else if(this.state.erroDataInicialSuperior) {
-            this.setState({ mensagemErro: "A data inicial é superior à data final!" })
-
-        // Mensagem de data final superior é renderizada caso a data final seja maior que a data atual.
-        } else if(this.state.erroDataFinalSuperior) {
-            this.setState({ mensagemErro: "A data final é superior à data atual!" })
-
-        // Nenhuma mensagem de erro é renderizada se os dois campos estão preenchidos corretamente.
-        } else {
-            this.setState({
-                mensagemErro: ""
-            }, () => { this.gerarExtrato() })
-        }
-
-    }
-
-    gerarExtrato = () => {
-        var dataInicio = this.state.dataInicio.replace(new RegExp('/', 'g'), '.');
-        var dataFim = this.state.dataFim.replace(new RegExp('/', 'g'), '.');
-        var empresa = localStorage.getItem("empresa");
-
-        PlanoService.RelatorioExtratoPorPlanoEmpresaReferencia(this.state.cdPlano, empresa, dataInicio, dataFim)
-            .then((result) => {
-                const url = window.URL.createObjectURL(new Blob([result.data]));
+            await this.validarData(this.state.dataInicio, "Data de Início");
+            await this.validarData(this.state.dataFim, "Data Fim");
+            
+            var dataInicio = this.converteData(this.state.dataInicio);
+            var dataFim = this.converteData(this.state.dataFim);
+    
+            if(dataInicio > dataFim)
+                this.alert.current.adicionarErro("A data inicial é superior à data final");
+    
+            if(dataFim > new Date())
+                this.alert.current.adicionarErro("A data final é superior à data atual");
+    
+    
+            if(this.alert.current.state.mensagem.length === 0 && this.alert.current.props.mensagem.length === 0) {
+                var dataInicio = this.state.dataInicio.replace(new RegExp('/', 'g'), '.');
+                var dataFim = this.state.dataFim.replace(new RegExp('/', 'g'), '.');
+                var empresa = localStorage.getItem("empresa");
+        
+                var { data: relatorio } = await PlanoService.RelatorioExtratoPorPlanoEmpresaReferencia(this.state.cdPlano, empresa, dataInicio, dataFim)
+    
+                const url = window.URL.createObjectURL(new Blob([relatorio]));
                 const link = document.createElement('a');
                 link.href = url;
                 link.setAttribute('download', 'Extrato.pdf');
                 document.body.appendChild(link);
                 link.click();
-            });
+            }
+            
+        } catch(err) {
+            console.error(err);
+        }
     }
+
+    validarData = async (data, dataObjeto, nomeCampo) => {
+        if(DataInvalida(dataObjeto, data))
+            await this.alert.current.adicionarErro(`Campo \"${nomeCampo}\" inválido.`);
+    }
+
+    converteData = (data) => {
+        var dataObjeto = data.split("/");
+        dataObjeto = new Date(dataObjeto[2], dataObjeto[1] - 1, dataObjeto[0]);
+        return dataObjeto;
+    }
+
 
     gerarCertificado = () => {
         var empresa = localStorage.getItem("empresa");
